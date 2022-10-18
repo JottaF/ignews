747c0d647ab78668245d488bc9f9144c4e9f6e2d
jottaf <joaofelipe.work@gmail.com>
2022-10-18 11:01:22 -0300

    Save the user email on FaunaDB

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,9 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 
+import { fauna } from "../../../services/fauna"
+import { query as q } from "faunadb"
+
 export const authOptions = {
     // Configure one or more authentication providers
     providers: [
@@ -10,6 +13,45 @@ export const authOptions = {
         }),
         // ...add more providers here
     ],
+    callbacks: {
+        async signIn({ user, account, profile }) {
+            const { email } = user
+            try {
+                await fauna.query(
+                    q.If(
+                        q.Not(
+                            q.Exists(
+                                q.Match(
+                                    q.Index('user_by_email'),
+                                    q.Casefold(email)
+                                )
+                            )
+                        ),
+                        q.Create(
+                            q.Collection('users'),
+                            { data: { email } }
+                        ),
+                        q.Get(
+                            q.Match(
+                                q.Index('user_by_email'),
+                                q.Casefold(email)
+                            )
+                        )
+                    )
+                )
+                return true
+
+            } catch (err) {
+                console.log(
+                    'Error: [%s] %s: %s',
+                    err.name,
+                    err.message,
+                    err.errors()[0].description,
+                )
+                return false
+            }
+        }
+    }
 }
 
 export default NextAuth(authOptions)
